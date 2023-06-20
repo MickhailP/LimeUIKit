9e0012310f7d060f8bfbf64b0fcded3f4e05a4f9
Mikhail <mich_p@inbox.ru>
2023-06-20 17:23:19 +0300

    Реализован сетевой слой, коды ошибок и эндпойнты. Добавлен  gitignore

---
+++ LimeUIKit/Services/Networking/Networking.swift
@@ -0,0 +1,94 @@
+//
+//  Networking.swift
+//  LimeTVStreaming
+//
+//  Created by Миша Перевозчиков on 01.12.2022.
+//
+
+import Foundation
+import UIKit
+
+
+final class NetworkService: NetworkingProtocol {
+
+	 let cache = ImageCacheManager.shared
+
+	 func downloadDataResult(from url: String, completionHandler: @escaping (Result<Data, Error>) -> Void) {
+
+
+		  guard let url = URL(string: url) else {
+				completionHandler(.failure(ErrorMessage.badURl))
+				return
+		  }
+
+		  let task = URLSession.shared.dataTask(with: url) { data, response, error in
+				if let error {
+					 completionHandler(.failure(error))
+				}
+
+				guard let data else {
+					 completionHandler(.failure(ErrorMessage.invalidData))
+					 return
+				}
+
+				do {
+					 try self.handleResponse(response)
+					 completionHandler(.success(data))
+
+				} catch  {
+					 print(error)
+					 print(error.localizedDescription)
+					 completionHandler(.failure(ErrorMessage.decodingError))
+				}
+		  }
+		  task.resume()
+	 }
+
+
+	 func downloadImage(from url: String) -> UIImage? {
+
+		  if let cachedImage = cache.get(key: url) {
+				return cachedImage
+		  } else {
+				var image: UIImage? = nil
+
+				fetchImage(from: url) { fetchedImage in
+					 image = fetchedImage
+				}
+
+				return image
+		  }
+	 }
+
+
+	 private func fetchImage(from urlString: String, completionHandler: @escaping (UIImage?) -> Void) {
+
+		  guard let url = URL(string: urlString) else {
+				completionHandler(nil)
+				return
+		  }
+
+		  let task = URLSession.shared.dataTask(with: url) { data, response, error in
+				if let error {
+					 completionHandler(nil)
+					 return
+				}
+
+				guard let data else {
+					 completionHandler(nil)
+					 return
+				}
+
+				do {
+					 try self.handleResponse(response)
+					 completionHandler(UIImage(data: data))
+
+				} catch  {
+					 print(error)
+					 print(error.localizedDescription)
+					 completionHandler(nil)
+				}
+		  }
+		  task.resume()
+	 }
+}
---
+++ LimeUIKit/Services/Networking/NetworkingProtocol.swift
@@ -0,0 +1,38 @@
+//
+//  NetworkingProtocol.swift
+//  LimeTVStreaming
+//
+//  Created by Миша Перевозчиков on 01.12.2022.
+//
+
+import Foundation
+import UIKit
+
+
+protocol NetworkingProtocol: AnyObject {
+
+	 func downloadDataResult(from url: String, completionHandler: @escaping (Result<Data, Error>) -> Void)
+	 func downloadImage(from url: String) -> UIImage?
+}
+
+
+extension NetworkingProtocol {
+
+	 /// Check if URLResponse have good status code, if it's not, it will throw an error
+	 /// - Parameter response: URLResponse from dataTask
+	 func handleResponse(_ response: URLResponse?) throws {
+
+		  guard let response = response as? HTTPURLResponse else {
+				throw URLError(.cannotParseResponse)
+		  }
+
+		  let statusCode = response.statusCode
+
+		  print(statusCode)
+		  if statusCode >= 200 && statusCode <= 300 {
+				return
+		  } else {
+				throw URLError(.badServerResponse)
+		  }
+	 }
+}
