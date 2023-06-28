abacb252568285538944b6f32a71e4b2f5095c8a
Mikhail <mich_p@inbox.ru>
2023-06-28 16:37:35 +0300

    ImageCacheManager

---
+++ LimeUIKit/Services/Persistance/ImageCacheManager.swift
@@ -0,0 +1,40 @@
+//
+//  ImageCacheManager.swift
+//  LimeUIKit
+//
+//  Created by Миша Перевозчиков on 20.06.2023.
+//
+
+import Foundation
+import UIKit
+
+
+//MARK: - ImageStorageProtocol
+protocol ImageStorageProtocol {
+
+	 func add(key: String, value: UIImage)
+	 func get(key: String) -> UIImage?
+}
+
+//MARK: - ImageCacheManager
+final class ImageCacheManager: ImageStorageProtocol {
+
+	 static let shared = ImageCacheManager()
+
+	 private init() { }
+
+	 var photoCache: NSCache<NSString, UIImage> = {
+		  var cache = NSCache<NSString, UIImage>()
+		  cache.countLimit = 200
+		  cache.totalCostLimit = 1024 * 1024 * 100 //100 MB
+		  return cache
+	 }()
+
+	 func add(key: String, value: UIImage) {
+		  photoCache.setObject(value, forKey: key as NSString)
+	 }
+
+	 func get(key: String) -> UIImage? {
+		  return photoCache.object(forKey: key as NSString)
+	 }
+}
